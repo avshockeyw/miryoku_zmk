a015fb2186973e61060058c929ae2d0cb4d2c4cf
avshockeyw <avshockeyw@users.noreply.github.com>
2022-12-14 12:39:38 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -16,7 +16,7 @@
  K40  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K44 \
  K41  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K45 \
  K42  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K46 \
-                     K34  K33  K35  K37  K36
+                     K33  K34  K35  K36  K37
   #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 36 37
   #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 39 40
      // based on orange keys:
@@ -52,7 +52,7 @@ MIRYOKU_X(FUN,    "Fun")
 #define MIRYOKU_LAYER_BASE \
 &kp TAB,    &kp Q,         &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,           &kp U,             &kp Y,             &kp SQT,          &kp BSPC,       \
 &kp LCTRL,  U_MT(LGUI, A), U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),  U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),    &kp SEMI        \
-&kp LSHFT,  &kp Z,         &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,           &kp COMMA,         &kp DOT,           &kp SLASH,        &u_to_U_BASE,   \
+&kp LSHFT,  &kp Z,         &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,           &kp COMMA,         &kp DOT,           &kp SLASH,        &kp RET,        \
 U_NP,       U_NP,          U_NP,              U_NP,              U_LT(U_MOUSE,ESC), U_LT(U_NAV,RET),   &kp BSPC,          U_LT(U_NUM,SPACE),U_LT(U_SYM,DEL),  U_NP,              U_NP,             U_NP 
 
 #define MIRYOKU_LAYER_EXTRA \

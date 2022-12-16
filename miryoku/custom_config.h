575e276248fb2cdd3cc42d489938eda2c84abde6
avshockeyw <avshockeyw@users.noreply.github.com>
2022-12-15 21:38:49 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -82,7 +82,7 @@ U_NP,       U_NP,          U_NP,              U_NP,              U_NA,          
 #define MIRYOKU_LAYER_MEDIA \
 U_NA,       U_BOOT,        &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      &kp C_VOL_UP,      U_RGB_TOG,         U_RGB_EFF,       U_RGB_HUI,         U_RGB_SAI,         U_RGB_BRI,        U_NA,           \
 U_NA,       &kp LGUI,      &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp C_VOL_DN,      U_EP_TOG,          &kp C_PREV,      &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,       U_NA,           \
-U_NA,       U_NA,          &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     &kp C_MUTE,        &u_out_tog,        &u_bt_sel_0,     &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,      U_NA,           \
+U_NA,       U_NA,          &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     &kp C_MUTE,        &u_out_tog,        &u_bt_sel_0,     &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,      &bt BT_CLR      \
 U_NP,       U_NP,          U_NP,              U_NP,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,        &kp C_MUTE,        U_NP,              U_NP,             U_NP
 
 #define MIRYOKU_LAYER_NUM \
